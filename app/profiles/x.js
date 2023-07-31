3aad9512232fe51a2702b3dd22f1bdae75bfbdc5
Dev-Siri <s0431668@gmail.com>
2023-08-01 01:23:13 +0530

    Prepare ext for release to chrome web store

--- app/profiles/x.js
+++ app/profiles/x.js
@@ -1,6 +1,8 @@
 // Script for https://twitter.com/X
 
 function replaceXRefs() {
+  if (document.title !== "Twitter (@twitter) / Twitter") return;
+
   // please don't even try to understand this, copied from the DOM
   const xTitle = document.querySelector(
     "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span > span:nth-child(1)"
@@ -9,7 +11,9 @@ function replaceXRefs() {
     "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1wbh5a2 > div > div > div > span"
   );
   const bio = document.querySelector("div[data-testid=UserDescription] > span");
-
+  const userUrl = document.querySelector(
+    "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div:nth-child(4) > div > a > span"
+  );
   // kill me
   const pfp = document.querySelector(
     "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1w6e6rj.r-1wtj0ep > div.css-1dbjc4n.r-1adg3ll.r-16l9doz.r-6gpygo.r-2o1y69.r-1v6e3re.r-bztko3.r-1xce0ei > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > a > div.css-1dbjc4n.r-kemksi.r-1wyvozj.r-633pao.r-u8s1d.r-1v2oles.r-desppf > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > img"
@@ -24,6 +28,8 @@ function replaceXRefs() {
     "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > a > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > div"
   );
 
+  if (userUrl)
+    userUrl.textContent = userUrl.textContent.replace("x.com", "twitter.com");
   if (xTitle) xTitle.textContent = "Twitter";
   if (xMention) xMention.textContent = "@twitter";
   if (pfp) pfp.src = chrome.runtime.getURL("images/twitter.avif");
@@ -45,4 +51,17 @@ function replaceXRefs() {
   }
 }
 
+function listener() {
+  const observer = new MutationObserver(replaceXRefs);
+  const titleElement = document.querySelector("title");
+
+  observer.disconnect();
+
+  replaceXRefs();
+
+  const config = { childList: true };
+  observer.observe(titleElement, config);
+}
+
 runFunction(replaceXRefs);
+runFunction(listener);
