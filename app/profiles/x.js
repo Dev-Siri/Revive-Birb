e53e566a9412c035887ad04ad54cedd68bbe3d3f
Dev-Siri <s0431668@gmail.com>
2023-07-31 19:31:55 +0530

    Improve x.profile.replacement.js to work correctly

---
+++ app/profiles/x.js
@@ -0,0 +1,48 @@
+// Script for https://twitter.com/X
+
+function replaceXRefs() {
+  // please don't even try to understand this, copied from the DOM
+  const xTitle = document.querySelector(
+    "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div > span > span:nth-child(1)"
+  );
+  const xMention = document.querySelector(
+    "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1wbh5a2 > div > div > div > span"
+  );
+  const bio = document.querySelector("div[data-testid=UserDescription] > span");
+
+  // kill me
+  const pfp = document.querySelector(
+    "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1w6e6rj.r-1wtj0ep > div.css-1dbjc4n.r-1adg3ll.r-16l9doz.r-6gpygo.r-2o1y69.r-1v6e3re.r-bztko3.r-1xce0ei > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > a > div.css-1dbjc4n.r-kemksi.r-1wyvozj.r-633pao.r-u8s1d.r-1v2oles.r-desppf > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > img"
+  );
+  const pfpDiv = document.querySelector(
+    "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1w6e6rj.r-1wtj0ep > div.css-1dbjc4n.r-1adg3ll.r-16l9doz.r-6gpygo.r-2o1y69.r-1v6e3re.r-bztko3.r-1xce0ei > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > a > div.css-1dbjc4n.r-kemksi.r-1wyvozj.r-633pao.r-u8s1d.r-1v2oles.r-desppf > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > div"
+  );
+  const banner = document.querySelector(
+    "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > a > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > img"
+  );
+  const bannerDiv = document.querySelector(
+    "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > a > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > div"
+  );
+
+  if (xTitle) xTitle.textContent = "Twitter";
+  if (xMention) xMention.textContent = "@twitter";
+  if (pfp) pfp.src = chrome.runtime.getURL("images/twitter.avif");
+  if (pfpDiv) {
+    const birdBgStyle = `background-image: url(${chrome.runtime.getURL(
+      "images/twitter.avif"
+    )}); background-color: #1cb8eb`;
+
+    pfpDiv.setAttribute("style", birdBgStyle);
+  }
+  if (bio) bio.textContent = "What's happening?";
+  if (banner) banner.src = chrome.runtime.getURL("images/banner.avif");
+  if (bannerDiv) {
+    const birdBgStyle = `background-image: url(${chrome.runtime.getURL(
+      "images/banner.avif"
+    )})`;
+
+    bannerDiv.setAttribute("style", birdBgStyle);
+  }
+}
+
+runFunction(replaceXRefs);
