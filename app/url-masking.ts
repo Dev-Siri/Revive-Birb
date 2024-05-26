6d0f1c9e00544aea1c9da1edfcd3e784ab1c51f9
Dev-Siri <s0431668@gmail.com>
2024-05-26 23:35:14 +0530

    redirect x.com to twitter.com links

--- app/url-masking.ts
+++ app/url-masking.ts
@@ -1,3 +1,9 @@
+export function replaceXwithTwitterDotCom() {
+  if (location.hostname !== "x.com") return;
+
+  location.href = `https://twitter.com${location.pathname}?mx=1`;
+}
+
 export default function maskUrls() {
   const pathname = location.pathname.toLowerCase();
 
